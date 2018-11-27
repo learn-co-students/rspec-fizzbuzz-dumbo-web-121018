5c04c1c71163cf698810b23315385a98b51a74a7
Ariel <arielord@buffalo.edu>
2018-11-27 18:41:43 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  fizz_or_buzz = ""
+  
+  if (num%3 == 0) && (num%5 == 0)
+    fizz_or_buzz = "FizzBuzz"
+  elsif num%3 == 0
+    fizz_or_buzz += "Fizz"
+  elsif num%5 == 0
+    fizz_or_buzz += "Buzz"
+  else
+    fizz_or_buzz = nil
+  end
+  
+  fizz_or_buzz
+end
