bc900b746b89d568e74efe8eeaf8c89d12609a34
github username <github email address>
2018-12-02 11:13:05 -0500

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
-# Don't forget! This file needs to be 'required' in its spec file
+def fizzbuzz (int)
+  #checks to see if the number is divisible by 3
+ if int % 15 == 0
+   "FizzBuzz"
+ else if int % 5 == 0
+    "Buzz"
+  else if int % 3 == 0
+     "Fizz"
+  else
+     nil
+     end
+
+ end
+ end
+end
 # See README.md for instructions on how to do this
